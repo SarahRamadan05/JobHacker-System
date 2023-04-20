847af196028a77dac59e6c2ec52517bf2a874890
SarahRamadan05 <sarahramadan84@yahoo.com>
2023-04-20 10:00:02 +1000

    solving Fibonacci Number problem

---
+++ Week-3/Week3Tasks/src/FibonacciNumber.java
@@ -0,0 +1,21 @@
+package week3tasks;
+import java.util.Scanner;
+public class FibonacciNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number ");
+        int num = scanner.nextInt();
+        int num1 =0;
+        int num2 =1;
+        System.out.println(num1+" "+num2);
+        int num3 = num1+num2;
+        while (num3<=num){
+            System.out.println(num3);
+            num1 = num2;
+            num2 =num3;
+            num3 = num1+num2;
+
+        }
+
+    }
+}
