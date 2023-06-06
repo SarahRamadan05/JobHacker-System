309984d292c2659b88ad3723b4e8f9f358e0dd35
SarahRamadan05 <sarahramadan84@yahoo.com>
2023-06-06 22:26:58 +1000

    Update fibonacci number task

--- Week-3/Week3Tasks/src/FibonacciNumber.java
+++ Week-3/Week3Tasks/src/FibonacciNumber.java
@@ -1,20 +1,25 @@
 import java.util.Scanner;
+
 public class FibonacciNumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a number ");
+        System.out.println("Enter a number: ");
         int num = scanner.nextInt();
-        int num1 =0;
-        int num2 =1;
-        System.out.println(num1+" "+num2);
-        int num3 = num1+num2;
-        while (num3<=num){
+
+        printFibonacciNumbers(num);
+    }
+
+    public static void printFibonacciNumbers(int num) {
+        int num1 = 0;
+        int num2 = 1;
+        System.out.println(num1 + " " + num2);
+
+        int num3 = num1 + num2;
+        while (num3 <= num) {
             System.out.println(num3);
             num1 = num2;
-            num2 =num3;
-            num3 = num1+num2;
-
+            num2 = num3;
+            num3 = num1 + num2;
         }
-
     }
 }
